1111c2e29cf036fb9be0570d11732a847c5b2ab1
Juan Ignacio Pesce <jipesce@gmail.com>
2022-08-28 17:24:47 -0400

    Cuarto commit

--- MVTJuanIgnacioPesce/views.py
+++ MVTJuanIgnacioPesce/views.py
@@ -1,34 +1,41 @@
 from django.http import HttpResponse
 from django.template import Template, Context, loader
 from Familiares.models import *
+from datetime import datetime
+from pytz import timezone
+
+FORMATO = "%Y-%m-%d %H:%M:%S"
 
 def saludo(request):
   return HttpResponse('Hola Django - Coder')
 
 def familiares(self):
 
+  BUENOS_AIRES_TZ = datetime.now(timezone('America/Argentina/Buenos_Aires'))
+
   diccionario = {
+    'FechaActual': BUENOS_AIRES_TZ,
     'Familiares': [
       {
       'Nombre': 'Silvina',
       'Apellido': 'Aguirre',
       'Parentesco': 'madre',
-      'Edad': 52,
-      'Nacimiento': {'anio': '1970', 'mes': '09', 'dia': '28' }
+      'Edad': 51,
+      'Nacimiento': {'anio': 1970, 'mes': '09', 'dia': '28' }
       },
       {
       'Nombre': 'Andres',
       'Apellido': 'Galeano',
       'Parentesco': 'padre',
       'Edad': 55,
-      'Nacimiento': {'anio': '1967', 'mes': '06', 'dia': '27' }
+      'Nacimiento': {'anio': 1967, 'mes': '06', 'dia': '27' }
       },
       {
       'Nombre': 'Ludmila',
       'Apellido': 'Galeano Aguirre',
       'Parentesco': 'hermana',
       'Edad': 25,
-      'Nacimiento': {'anio': '1997', 'mes': '03', 'dia': '24' }
+      'Nacimiento': {'anio': 1997, 'mes': '03', 'dia': '24' }
       }
     ]
   }
